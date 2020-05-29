cf4e88b57d85314e9fe5f3349919500a538abffa
Dor <dormesika2@gmail.com>
2020-05-29 16:20:26 +0300

    Init Commit

---
+++ src/utils/users.js
@@ -0,0 +1,58 @@
+const users = []
+
+// addUser, removeUser, getUser, getUsersInRoom
+
+const addUser = ({ id, username, room }) => {
+  // Clean the data
+  username = username.trim().toLowerCase()
+  room = room.trim().toLowerCase()
+
+  // Validate the data
+  if (!username || !room) {
+    return {
+      error: 'Username and room are required'
+    }
+  }
+
+  // Check for existing user
+  const existingUser = users.find((user) => {
+    return user.room === room && user.username === username
+  })
+
+  // Validate username
+  if (existingUser) {
+    return {
+      error: 'Username is in use!'
+    }
+  }
+
+  // Store user
+  const user = { id, username, room }
+  users.push(user)
+  return { user }
+}
+
+const removeUser = (id) => {
+  const index = users.findIndex((user) => user.id === id)
+
+  if(index !== -1) {
+    return users.splice(index, 1)[0] //Faster than filter (finds and stops)
+  }
+}
+
+const getUser = (id) => { //Array Find Method
+  const user = users.find((user) => user.id === id)
+  return user
+}
+
+const getUsersInRoom = (room) => { //Return array of users (Array Filter Method)
+  room = room.trim().toLowerCase()
+  return users.filter((user) => user.room === room)
+}
+
+module.exports = {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom
+}
